221768c518b071da0a4422dc5d5f3c5a5964175d
lib26 <dladlsqja@naver.com>
2023-01-30 15:16:45 +0900

    test: 자동 빈 등록과 수동 빈 등록이 동시에 같은 이름을 쓴다면?

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -14,4 +17,14 @@ import static org.springframework.context.annotation.ComponentScan.*;
 @ComponentScan(excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 public class AutoAppConfig {
     // 기존 AppConfig와 다르게 수동으로 @Bean 등록을 해준 것이 아무것도 없다.
+
+    // 근데 만약 수동으로 빈을 설정하는데, 컴포넌트 스캔으로 자동 빈 등록되어있는 빈이름과 중복되는 이름으로 한다면?
+    // 수동으로 반영이된다. 즉 수동으로 해준 빈으로 등록이 오버라이딩된다.
+    // Test는 AutoAppConfig에서 돌려보면 알 수 있다,
+    // 하지만 또 스프링 부트는 자동 빈 등록 VS 수동 빈 등록 에 대해서 중복되는 빈이름이 있다면 오류를 발생시킨다.
+    // 이 오류를 끄고싶다면 application.properties에서 spring.main.allow-bean-definition-overriding=true 를 해주면 된다.
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
