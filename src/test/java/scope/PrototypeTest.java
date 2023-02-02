0690911fdcb05461171a270966e0a677aff427ee
lib26 <dladlsqja@naver.com>
2023-02-02 23:48:46 +0900

    test: 잘못된 prototype bean 사용

--- src/test/java/scope/PrototypeTest.java
+++ src/test/java/scope/PrototypeTest.java
@@ -6,18 +6,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class PrototypeTest {
     @Test
     public void prototypeBeanFind() {
+        // 컨테이너가 만들어지고
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
 
         System.out.println("find prototypeBean1");
-        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class); // 클라이언트가 요청할 때 새로운 빈을 생성하여 DI 후 반환한다
         System.out.println("find prototypeBean2");
-        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class); // 클라이언트가 요청할 때 새로운 빈을 생성하여 DI 후 반환한다
 
         System.out.println("prototypeBean1 = " + prototypeBean1);
         System.out.println("prototypeBean2 = " + prototypeBean2);
@@ -27,6 +29,7 @@ public class PrototypeTest {
     }
 
     @Scope("prototype") // 컨테이너가 빈 생성, 의존성 주입, 초기화 콜백까지만 관리한다
+    // @Component 위에서 컨테이너 생성할 때 파라미터로 넣어주기 때문에 생략 가능. 즉, 자동으로 componentScan 대상으로 포함된다.
     static class PrototypeBean {
         @PostConstruct // 초기화 콜백
         public void init() {
--- src/test/java/scope/SingletonTest.java
+++ src/test/java/scope/SingletonTest.java
@@ -12,6 +12,7 @@ public class SingletonTest {
 
     @Test
     public void singletonBeanFind() {
+        // 프로토타입 빈과 다르게 싱클톤 빈은 컨테이너가 만들어지고 빈을 바로 등록하는 것 같다
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
 
         SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
@@ -23,8 +24,9 @@ public class SingletonTest {
     }
 
     @Scope("singleton") // 디폴트라 안해도 되긴하다
+    // @Component 위에서 컨테이너 생성할 때 파라미터로 넣어주기 때문에 생략 가능. 즉, 자동으로 componentScan 대상으로 포함된다.
     static class SingletonBean {
-        @PostConstruct // 빈 생성, 의존성 주입 이후 호출되는 초기활 콜백 메소드
+        @PostConstruct // 빈 생성, 의존성 주입 이후 호출되는 초기화 콜백 메소드
         public void init() {
             System.out.println("SingletonBean.init");
         }
